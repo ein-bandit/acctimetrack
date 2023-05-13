60a2b1f98a1dc717648077fe3e512296d4576e53
kryptografisch <code@kryptografische.biz>
2023-05-13 22:04:26 +0200

    changed header styling

--- islands/Header.tsx
+++ islands/Header.tsx
@@ -14,8 +14,8 @@ export default function Header({ active }: Props) {
   return (
     <div class="bg-white w-full max-w-screen-lg py-6 px-8 flex flex-col md:flex-row gap-4">
       <div class="flex items-center flex-1">
-        <IconCalendarTime />
-        <div class="text-2xl  ml-1 font-bold">
+        <IconCalendarTime size={28} />
+        <div class="text-2xl  ml-4 font-bold">
           ACC Time Track
         </div>
       </div>
